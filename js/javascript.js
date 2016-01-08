e0bc3576c83e6fdefaf383a9a53b15d8f2e3fed1
karldiab <karldiab@live.com>
2016-01-08 00:18:26 -0800

    started project by formatting time and date display. Started slowly on time input, still trying to decide how to format the inputs

---
+++ js/javascript.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+// Create two variable with the names of the months and days in an array
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+var am = true;
+
+// Create a newDate() object
+var newDate = new Date();
+// Extract the current date from Date object
+newDate.setDate(newDate.getDate());
+// Output the day, date, month and year   
+$('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+
+setInterval( function() {
+	// Create a newDate() object and extract the seconds of the current time on the visitor's
+	var seconds = new Date().getSeconds();
+	// Add a leading zero to seconds value
+	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+	},1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the minutes of the current time on the visitor's
+	var minutes = new Date().getMinutes();
+	// Add a leading zero to the minutes value
+	$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    },1000);
+	
+setInterval( function() {
+	// Create a newDate() object and extract the hours of the current time on the visitor's
+	var hours = new Date().getHours();
+	//convert hours to am/pm time
+    if (hours > 12) {
+        hours -= 12;
+        am = false;
+    } else {
+        am = true;
+    }
+    // Add a leading zero to the hours value
+	$("#hours").html(hours == 0 ? "12" : (( hours < 10 ? "0" : "" ) + hours));
+    }, 1000);	
+    
+setInterval( function() {
+    if (am) {
+        $("#ampm").html("AM");
+    } else {
+        $("#ampm").html("PM");
+    }
+    },1000);
+    
+    
+});
+
