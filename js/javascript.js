4099f16bbe619bfd25ab49bc16882b80a77efd9f
Karl Diab <A00805534@ad.bcit.ca>
2016-01-08 16:30:52 -0800

    Finished first working version. Now only cosmetic changes needed

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
 var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
 var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var am = true;
-
+var minutes;
+var hours;
+var hoursTillAlarm
+var minutesTillAlarm
+var alarmSet = false;
+var URL;
 // Create a newDate() object
 var newDate = new Date();
 // Extract the current date from Date object
@@ -16,18 +21,20 @@ setInterval( function() {
 	var seconds = new Date().getSeconds();
 	// Add a leading zero to seconds value
 	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+    //updates alarm
+    updateAlarm();
 	},1000);
 	
 setInterval( function() {
 	// Create a newDate() object and extract the minutes of the current time on the visitor's
-	var minutes = new Date().getMinutes();
+	minutes = new Date().getMinutes();
 	// Add a leading zero to the minutes value
 	$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
     },1000);
 	
 setInterval( function() {
 	// Create a newDate() object and extract the hours of the current time on the visitor's
-	var hours = new Date().getHours();
+	hours = new Date().getHours();
 	//convert hours to am/pm time
     if (hours > 12) {
         hours -= 12;
@@ -37,6 +44,10 @@ setInterval( function() {
     }
     // Add a leading zero to the hours value
 	$("#hours").html(hours == 0 ? "12" : (( hours < 10 ? "0" : "" ) + hours));
+    //change back to 24 hour time for alarm function
+    if (!am) {
+        hours += 12;
+    }
     }, 1000);	
     
 setInterval( function() {
@@ -46,7 +57,37 @@ setInterval( function() {
         $("#ampm").html("PM");
     }
     },1000);
-    
-    
-});
+       
+//function to set alarm when set alarm button clicked  
+$('#setAlarmButton').click(function() {
+    URL = $("#URL").val();
+    alarmSet = true;
+    updateAlarm();
 
+});
+/*function that updates how many hours and minutes left till alarm sounds, updates the text display said info, and redirrects page when
+the alarm time == the current time */
+function updateAlarm() {
+    var alarmHour = $("#hour").find(":selected").val();
+    var alarmMinute = $("#minute").find(":selected").val();
+    hoursTillAlarm = alarmHour - hours;
+    if (hoursTillAlarm < 0) {
+        hoursTillAlarm += 24;
+    }
+    minutesTillAlarm = alarmMinute - minutes;
+    if (minutesTillAlarm < 0) {
+        minutesTillAlarm += 60;
+        hoursTillAlarm--;
+    }
+    if (hoursTillAlarm == -1) {
+        hoursTillAlarm = 23;
+    }
+    if (alarmSet) {
+        $("#alarmConfirm").html("You will be sent to that link in " + (hoursTillAlarm == 0? "" : hoursTillAlarm + (hoursTillAlarm == 1? " hour and " : " hours and ")) + minutesTillAlarm + (minutesTillAlarm == 1 ? " minute." : " minutes."));
+    }
+    if (alarmSet && hoursTillAlarm == 0 && minutesTillAlarm == 0) {
+        window.location.href = URL;
+    }
+    
+}
+});
